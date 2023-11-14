487647aed8e9ea6174a3ca22eefbd29a9c323deb
arkaeilion <arkaeilion@gmail.com>
2023-11-14 18:21:41 +1100

    animating sprite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+use bevy::{prelude::*, window::{WindowResolution, PresentMode}};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Toy Farm".to_string(),
+                        resolution: WindowResolution::new(1920.0, 1080.0),
+                        present_mode: PresentMode::AutoVsync,
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_sprite)
+        .run();
+}
+
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    let texture_handle = asset_server.load("sprite-sheet.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 128, 128, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    let animation_indices = AnimationIndices {
+        first: 1,
+        last: 1000,
+    };
+
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(animation_indices.first),
+            transform: Transform::from_xyz(100., 0., 0.),
+            ..default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.01, TimerMode::Repeating)),
+    ));
+}
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = if sprite.index == indices.last {
+                indices.first
+            } else {
+                sprite.index + 1
+            };
+        }
+    }
 }
